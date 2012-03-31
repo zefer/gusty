23b2fd3054bd03d6483e760e40616c6e08bcaf91
Joe Roberts <joe@zefer.co.uk>
2012-03-31 17:04:38 +0100

    Twitter authentication using oauth-twitter - no session/persistence or usage of the user yet

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ gem 'sinatra'
 gem 'thin'
 gem 'mustache'
 
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'multi_json'
+
 group :development, :test do
   gem 'rake'
   gem 'foreman'
--- app.rb
+++ app.rb
@@ -1,18 +1,37 @@
 require 'sinatra/base'
 require 'mustache/sinatra'
+require 'omniauth'
+require 'omniauth-twitter'
 
 class App < Sinatra::Base
   register Mustache::Sinatra
   require './views/layout'
+
+  # Load env vars from local file - for local dev
+  load("./config/local_env.rb") if File.exists?("./config/local_env.rb")
 
   set :mustache, {
     views:     './views',
     templates: './templates'
   }
 
+  use Rack::Session::Cookie
+  use OmniAuth::Builder do
+    provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
+  end
+
   get '/' do
     @title = "Gusty: An experiment"
     mustache :index
   end
+
+  twitter_auth_callback = lambda do
+  	auth = request.env['omniauth.auth']
+
+    # https://github.com/intridea/omniauth/wiki/Auth-Hash-Schema
+    puts auth[:info][:nickname]
+	end
+	get  '/auth/:name/callback', &twitter_auth_callback
+	post '/auth/:name/callback', &twitter_auth_callback
 
 end
